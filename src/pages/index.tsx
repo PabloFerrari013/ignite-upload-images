8867f9f4c17d4788fdc519bb662f3ee3d6ac2337
Pablo Ferrari <pabloferraricaliari@gmail.com>
2023-04-27 10:36:15 -0300

    configuração de ambiente, e começo de edição da pagina index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
-import { Button, Box } from '@chakra-ui/react';
-import { useMemo } from 'react';
-import { useInfiniteQuery } from 'react-query';
+import { Button, Box } from "@chakra-ui/react";
+import { useMemo } from "react";
+import { useInfiniteQuery } from "react-query";
 
-import { Header } from '../components/Header';
-import { CardList } from '../components/CardList';
-import { api } from '../services/api';
-import { Loading } from '../components/Loading';
-import { Error } from '../components/Error';
+import { Header } from "../components/Header";
+import { CardList } from "../components/CardList";
+import { api } from "../services/api";
+import { Loading } from "../components/Loading";
+import { Error } from "../components/Error";
+
+interface useInfiniteQueryResponse {
+  data: {
+    pages: {
+      data: {
+        title: string;
+        description: string;
+        url: string;
+        ts: string;
+        id: string;
+      };
+      after: string;
+    }[];
+  };
+}
 
 export default function Home(): JSX.Element {
   const {
@@ -16,15 +31,24 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+  } = useInfiniteQuery<useInfiniteQueryResponse>(
+    "images",
+    ({ pageParam = null }) => api.get(`/api/images?after=${pageParam}`),
+    {
+      getNextPageParam: (lastPage) => {
+        console.log(lastPage);
+
+        if (!lastPage?.data?.pages[0].after) return null;
+        return lastPage.data.pages[0].after;
+      },
+    }
   );
 
   const formattedData = useMemo(() => {
+    console.log(data);
+
     // TODO FORMAT AND FLAT DATA ARRAY
+    console.log(data);
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
@@ -33,11 +57,12 @@ export default function Home(): JSX.Element {
 
   return (
     <>
-      <Header />
+      <h1>Hello World</h1>
+      {/* <Header /> */}
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE*/}
       </Box>
     </>
   );
